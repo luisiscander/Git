cb5bfb690a104a28601eb8a57bd0ad0897e5eb62
Luiso <luisiscander2022@gmail.com>
2025-08-26 22:18:26 -0500

    initial commit

--- app/src/main/java/com/example/git_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/git_tutorial/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GittutorialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
